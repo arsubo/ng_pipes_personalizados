1ac4b57c14feb322b553413d50a6a24dc201685d
Arnoldo Suarez <arsubo@gmail.com>
2026-05-22 11:50:04 -0600

    fix: ocultar rutas dinamicas y comodines de la barra de navegacion

--- src/app/components/navbar.component/navbar.component.ts
+++ src/app/components/navbar.component/navbar.component.ts
@@ -8,8 +8,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-  routes = routes.map((route) => ({
-    title: route.title ?? '',
-    path: route.path ?? '',
-  }));
+  routes = routes
+    .filter((route) => route.title && route.path && !route.path.includes(':') && route.path !== '**')
+    .map((route) => ({
+      title: route.title ?? '',
+      path: route.path ?? '',
+    }));
 }
